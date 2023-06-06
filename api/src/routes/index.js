e72e90f1663eabe20746e59ff538f8237c9aa95f
Hernando Rey <hrking31@gmail.com>
2023-06-05 21:55:12 -0500

    configurando cors

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
+const cors = require("cors");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const pokemonsRouter = require("./pokemonsRouter");
 const typesRouter = require("./typesRouter");
 
 const router = Router();
+router.use(cors());
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
